477a85f6216076f9d71a91aeb59448b9235d048b
JohnLee <>
2020-06-23 20:33:52 +0800

    no message

--- assets/Script/config/PhysicsManager.js
+++ assets/Script/config/PhysicsManager.js
@@ -15,6 +15,13 @@ cc.Class({
             type:cc.Boolean,
             tooltip:"是否开启物理绘制调试信息"
         },
+
+// default: 设置属性的默认值，这个默认值仅在组件 第一次 添加到节点上时才会用到
+// type: 限定属性的数据类型，详见 CCClass 进阶参考：type 参数
+// visible: 设为 false 则不在 属性检查器 面板中显示该属性
+// serializable: 设为 false 则不序列化（保存）该属性
+// displayName: 在 属性检查器 面板中显示成指定名字
+// tooltip: 在 属性检查器 面板中添加属性的 Tooltip
     },
 
     // LIFE-CYCLE CALLBACKS:
