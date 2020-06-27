c2fd253ecdfa000aa386a3706501196134c8d739
Lee0508 <liqiang497232807@gmail.com>
2020-06-27 14:56:36 +0800

    back

--- build/jsb-default/frameworks/runtime-src/Classes/AppDelegate.cpp
+++ build/jsb-default/frameworks/runtime-src/Classes/AppDelegate.cpp
@@ -34,7 +34,7 @@
 #include "cocos/scripting/js-bindings/manual/jsb_classtype.hpp"
 
 USING_NS_CC;
-
+using namespace std;
 AppDelegate::AppDelegate(int width, int height) : Application("Cocos Game", width, height)
 {
 }
@@ -45,6 +45,18 @@ AppDelegate::~AppDelegate()
 
 bool AppDelegate::applicationDidFinishLaunching()
 {
+    //packages path
+    string tDir = FileUtils::getInstance()->getWritablePath();
+    FileUtils::getInstance()->createDirectory(tDir + "package/");
+    FileUtils::getInstance()->createDirectory(tDir + "packageTemp/");
+    FileUtils::getInstance()->createDirectory(tDir + "config/");
+
+
+    FileUtils::getInstance()->addSearchPath(tDir + "package/src/",true);
+    FileUtils::getInstance()->addSearchPath(tDir + "package/res/",true);
+    FileUtils::getInstance()->addSearchPath(tDir + "package/",true);
+
+
     se::ScriptEngine* se = se::ScriptEngine::getInstance();
 
     jsb_set_xxtea_key("e2ededca-352b-49");
