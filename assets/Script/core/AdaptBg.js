a9cdfa4b21b2657093d8fe2f6d0b2d6b3aaa1be6
Lee0508 <liqiang497232807@gmail.com>
2020-08-27 13:57:01 +0800

    适配修改

--- assets/Script/core/AdaptBg.js
+++ assets/Script/core/AdaptBg.js
@@ -1,3 +1,10 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: Lee
+ * @Date: 2020-06-28 10:52:53
+ * @LastEditTime: 2020-08-27 11:50:44
+ */
 
 cc.Class({
     extends: cc.Component,
@@ -16,13 +23,18 @@ cc.Class({
     },
 
     start() {
+        cc.find('Canvas').on('resize',this.resize.bind(this));
         
+        this.resize()
+    },
+
+    resize(){
         var size  = cc.view.getVisibleSize()
 
         var scale = Math.min(this.node.width/size.width,this.node.height/size.height)
         
         this.node.scale =  this.node.scale/scale
-
-    },
+        
+    }
 
 });
