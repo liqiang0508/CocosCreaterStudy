a3346ffc5b7f2a81287ec8f95b22f357206437a8
Lee <497232807@qq.com>
2022-07-29 16:29:56 +0800

    bugly

---
+++ build/jsb-default/frameworks/runtime-src/proj.android-studio/app/src/com/casino/game/BuglyUtils.java
@@ -0,0 +1,32 @@
+package com.casino.game;
+
+import android.content.Context;
+import android.os.Build;
+import android.util.Log;
+
+import com.tencent.bugly.crashreport.CrashReport;
+
+import java.util.Map;
+
+/**
+ * author : Admin
+ * date : 2022/7/29 9:47
+ * description :
+ */
+public class BuglyUtils {
+    public static void initSDK(Context context,String appId)
+    {
+        CrashReport.initCrashReport(context);
+        CrashReport.setDeviceModel(context, Build.MODEL);
+        Log.i("BuglyUtils","initSDK");
+    }
+
+    public static void postException(int category, String name, String reason, String stack){
+        Log.i("BuglyUtils","postException");
+        postException(category, name, reason, stack, null);
+    }
+
+    public static void postException(int category, String name, String reason, String stack, Map<String, String> extraInfo){
+        CrashReport.postException(category, name, reason, stack, extraInfo);
+    }
+}
